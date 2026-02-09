e75bc6ebbcd194d52e4d093eb039568a2123c851
Aurélien Tomassini <aurelien.tomassini+ei@gmail.com>
2026-02-09 19:07:56 +0100

    Prepare next version

--- Recalbox/userscripts/home_assistant_notifier.sh
+++ Recalbox/userscripts/home_assistant_notifier.sh
@@ -4,7 +4,7 @@
 # A placer dans le dossier userscripts
 # Par Aurélien Tomassini
 
-SCRIPT_VERSION="home_assistant_notifier.sh:v1.6.2"
+SCRIPT_VERSION="home_assistant_notifier.sh:v1.6.3"
 
 # Configuration
 HOME_ASSISTANT_DOMAIN="homeassistant.local"
